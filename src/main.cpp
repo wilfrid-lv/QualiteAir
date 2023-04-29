adc738706f69a539f4d4ea0bb297452e108b5533
wilfrid-lv <wilfrid-levalegant@live.fr>
2023-04-29 15:22:26 -0400

    correction, le capteur marche et envoie au mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "myFunctions.cpp"
 #include <WifiManager.h>
 #include <Wire.h>
+#include <Arduino.h>
 
 #define MQTT_SERVER "172.16.5.202" // Adresse IP du broker MQTT
 #define MQTT_PORT 1883 // Port utilisé par le broker MQTT
@@ -12,11 +13,13 @@
 #define MQTT_MOTDEPASSE "Patate123"
 const char* mqtt_server = "172.16.5.202";
 
+// Définition des variables nécessaires à la communication MQTT
 WiFiManager wm;
 Adafruit_CCS811 ccs;
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
+// Définition des variables nécessaires à la connexion au réseau WiFi
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
@@ -27,6 +30,7 @@ const char * SSID = "EcoleDuWeb2.4g";
 const char * PASSWORD = "EcoleDuWEB";
 
 
+// Fonction permettant de se connecter au réseau WiFi
 void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
@@ -47,7 +51,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-
+// Fonction de callback appelée lorsqu'un message est reçu sur le broker MQTT
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
@@ -60,8 +64,8 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
   Serial.println();
 
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off".
-  // Changes the output state according to the message
+  // Si un message est reçu sur le topic esp32/output, on vérifie si le message est "on" ou "off".
+  // On change l'état de la sortie en fonction du message
   if (String(topic) == "esp32/output") {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
@@ -73,11 +77,12 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
 }
 
+// Fonction permettant de se reconnecter au broker MQTT
 void reconnect() {
-  // Loop until we're reconnected
+ // Boucle jusqu'à ce que la reconnexion soit établie
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
+    // Tentative de connexion
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
       // Subscribe
@@ -86,7 +91,6 @@ void reconnect() {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -94,33 +98,27 @@ void reconnect() {
 
 void setup() {
   Serial.begin(9600);
+  // Initialisation du capteur CCS811
   if(!ccs.begin()){
     Serial.println("Failed to start sensor!");
     while(1);
   }
 
   setup_wifi();
+  // Configuration du client MQTT pour se connecter au serveur MQTT à l'adresse mqtt_server sur le port 1883
   client.setServer(mqtt_server, 1883);
+  // Configuration de la fonction de rappel callback() qui sera appelée lorsque des messages seront reçus sur les topics MQTT
   client.setCallback(callback);
 
   //Connection au WifiManager
-  // String ssIDRandom, PASSRandom;
-  // String stringRandom;
-  // stringRandom = get_random_string(4).c_str();
-  // ssIDRandom = SSID;
-  // ssIDRandom = ssIDRandom + stringRandom;
-  // stringRandom = get_random_string(4).c_str();
-  // PASSRandom = PASSWORD;
-  // PASSRandom = PASSRandom + stringRandom;
+  char strToPrint[128];
+  sprintf(strToPrint, "Identification : %s   MotDePasse: %s", SSID, PASSWORD);
+  Serial.println(strToPrint);
 
-  // char strToPrint[128];
-  // sprintf(strToPrint, "Identification : %s   MotDePasse: %s", ssIDRandom, PASSRandom);
-  // Serial.println(strToPrint);
-
-  // if (!wm.autoConnect(ssIDRandom.c_str(), PASSRandom.c_str())) {
-  //   Serial.println("Erreur de connexion.");
-  // } else {
-  //   Serial.println("Connexion Établie.");
+  if (!wm.autoConnect(SSID, PASSWORD)) {
+    Serial.println("Erreur de connexion.");
+  } else {
+    Serial.println("Connexion Établie.");
 
   //   // Connection au broker MQTT
   //   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
@@ -137,7 +135,7 @@ void setup() {
   //   }
   // }
 }
-
+}
 void loop() {
 
   if (!client.connected()) {
@@ -146,6 +144,7 @@ void loop() {
   client.loop();
 
   long now = millis();
+  // Publie les données sur le topic
   if (now - lastMsg > 5000) {
     lastMsg = now;
    
@@ -155,24 +154,21 @@ void loop() {
 
     //Serial.println(ccs.geteCO2);
   }
-
+// Vérifie si de nouvelles données sont disponibles sur le capteur CCS811
   if(ccs.available()){
+    ccs.readData();
     float CO2 = ccs.geteCO2();
     float TVOC = ccs.getTVOC();
     Serial.print("eCO2: ");
     Serial.print(CO2);
-    Serial.print(" ppm, TVOC: ");
-    Serial.print(TVOC);
-    Serial.println(" ppb");
+    Serial.print(" ppm");
 
     // Publication des données sur le topic MQTT
-  //   char message[64];
-  //   sprintf(message, "{\"CO2\": %f, \"TVOC\": %f}", CO2, TVOC);
-  //   mqttClient.publish(MQTT_TOPIC, message);
-  // }
-  // else{
-  //   Serial.println("ERROR!");
+    char message[64];
+    sprintf(message, "{\"CO2\": %f}", CO2);
+    mqttClient.publish(MQTT_TOPIC, message);
+   }
+  delay (1000);
   }
-  delay(1000);
-}
 // Ce code renvoie sur le terminal le taux de co2 ainsi que le TVOC, et publie les données sur un broker MQTT.
+//il manque plus qu'a rajoute une led pour un retour de l'app au esp en passant par le broker
